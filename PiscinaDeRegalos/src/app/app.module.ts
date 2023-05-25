53d2ea84c8e52666b835c8cc15a2c006a84a4c69
Victor Raul Castro Mier <viracami3@gmail.com>
2023-05-25 17:18:06 -0500

    se añade barra

--- PiscinaDeRegalos/src/app/app.module.ts
+++ PiscinaDeRegalos/src/app/app.module.ts
@@ -1,30 +1,26 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 import { HttpClientModule } from '@angular/common/http';
-
 import {DragDropModule} from '@angular/cdk/drag-drop';
 
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
-//import { FlexLayoutModule } from '@angular/flex-layout';
-// import { NgxDragDropModule } from 'ngx-drag-drop';
+
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { AgregarRegaloComponent } from './agregar-regalo/agregar-regalo.component';
 import { ListarRegalosComponent } from './listar-regalos/listar-regalos.component';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { AsignarRegalosComponent } from './asignar-regalos/asignar-regalos.component';
+
+import { CommonModule } from '@angular/common';
+import { AngularMaterialModule } from './AngularMaterialModule';
+
 
 @NgModule({
   declarations: [
@@ -34,31 +30,26 @@ import { AsignarRegalosComponent } from './asignar-regalos/asignar-regalos.compo
     AsignarRegalosComponent
   ],
   imports: [
+    CommonModule,
     FormsModule,
-    DragDropModule,
-    //NgxDragDropModule,
-    // FlexLayoutModule ,
+    DragDropModule,    
     BrowserModule,
     AppRoutingModule,
 
-    MatTableModule,
-    //MatTableDataSource,
-    BrowserAnimationsModule,
+
+    AngularMaterialModule,
+
     ReactiveFormsModule,
     HttpClientModule,
+    
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
+    // provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage()),
-    // DndModule,
-    
-    
-    
+    provideStorage(() => getStorage()),    
   ],
   providers: [
     { provide: FIREBASE_OPTIONS, useValue: environment.firebase }
-
     
   ],
   bootstrap: [AppComponent]
